5e038786cd504868d261e2d313ef79d14dc9d557
tanmv <tanmv@emotiv.com>
2019-03-14 18:19:06 +0700

    full option api swagger

--- app/Http/Controllers/TestApiController.php
+++ app/Http/Controllers/TestApiController.php
@@ -13,8 +13,30 @@ class TestApiController extends Controller {
      *     summary="Get list users",
      *     description="Returns list users",
      *     @OA\Response(
+     *         response="default",
      *         response=200,
-     *         description="List users"
+     *         description="New user info",
+     *         @OA\JsonContent(
+     *            type="array",
+     *            @OA\Items(
+     *              @OA\Property(
+     *                  property="id",
+     *                  type="integer",
+     *                  format="int32",
+     *                  example=1
+     *              ),
+     *              @OA\Property(
+     *                  property="name",
+     *                  type="string",
+     *                  example="Tan"
+     *              ),
+     *              @OA\Property(
+     *                  property="address",
+     *                  type="string",
+     *                  example="Hanoi"
+     *              )
+     *            )
+     *         )
      *      ),
      *      @OA\Response(response=400, description="Bad request"),
      *      security={
@@ -32,6 +54,58 @@ class TestApiController extends Controller {
     }
 
     /**
+     * @OA\Get(
+     *     path="/users/{id}",
+     *     operationId="getUsersDetail",
+     *     tags={"Example"},
+     *     summary="Get user detail",
+     *     description="Returns user info",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="query",
+     *         description="User_id",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *          response=200,
+     *          description="User info",
+     *          @OA\JsonContent(
+     *              @OA\Property(
+     *                  property="id",
+     *                  type="integer",
+     *                  format="int32",
+     *                  example=1
+     *              ),
+     *              @OA\Property(
+     *                  property="name",
+     *                  type="string",
+     *                  example="Tan"
+     *              ),
+     *              @OA\Property(
+     *                  property="address",
+     *                  type="string",
+     *                  example="Hanoi"
+     *              )
+     *         )
+     *      ),
+     *      security={
+     *           {"api_key_security_example": {}}
+     *      }
+     *    )
+     *
+     */
+    public function Info($id) {
+        return [
+            'error' => 0,
+            'message' => 'Done',
+            'list' => $id
+        ];
+    }
+
+    /**
      * @OA\Post(
      *     path="/users",
      *     operationId="addUsers",
@@ -39,11 +113,40 @@ class TestApiController extends Controller {
      *     summary="Add new users",
      *     description="Add new users",
      *
+     *     @OA\RequestBody(
+     *         description="User object info",
+     *         required=true,
+     *         @OA\JsonContent(
+     *            @OA\Property(
+     *              property="name",
+     *              type="string",
+     *              default=""
+     *            ),
+     *            @OA\Property(
+     *              property="address",
+     *              type="string"
+     *            )
+     *         )
+     *     ),
+     *
      *     @OA\Response(
      *         response=200,
-     *         description="New user info"
+     *         description="New user info",
+     *         @OA\JsonContent(
+     *            @OA\Property(
+     *              property="id",
+     *              type="integer"
+     *            ),
+     *            @OA\Property(
+     *              property="name",
+     *              type="string"
+     *            ),
+     *            @OA\Property(
+     *              property="address",
+     *              type="string"
+     *            )
+     *         )
      *      ),
-     *      @OA\Response(response=400, description="Bad request"),
      *      security={
      *          {"api_key_security_example": {}}
      *      }
@@ -51,6 +154,123 @@ class TestApiController extends Controller {
      *
      */
     public function Add() {
+        return [
+            'error' => 0,
+            'message' => 'Done',
+            'info' => [
+                'id' => 1,
+                'name' => 'A',
+                'address' => 'hanoi'
+            ]
+        ];
+    }
+
+    /**
+     * @OA\Put(
+     *     path="/users/{id}",
+     *     operationId="updateUsers",
+     *     tags={"Example"},
+     *     summary="Update users",
+     *     description="update users",
+     *
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="query",
+     *         description="User_id",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *
+     *     @OA\RequestBody(
+     *         description="User object info",
+     *         required=true,
+     *         @OA\JsonContent(
+     *            @OA\Property(
+     *              property="name",
+     *              type="string",
+     *              default=""
+     *            ),
+     *            @OA\Property(
+     *              property="address",
+     *              type="string"
+     *            )
+     *         )
+     *     ),
+     *
+     *     @OA\Response(
+     *         response=200,
+     *         description="New user info",
+     *         @OA\JsonContent(
+     *            @OA\Property(
+     *              property="id",
+     *              type="integer"
+     *            ),
+     *            @OA\Property(
+     *              property="name",
+     *              type="string"
+     *            ),
+     *            @OA\Property(
+     *              property="address",
+     *              type="string"
+     *            )
+     *         )
+     *      ),
+     *      security={
+     *          {"api_key_security_example": {}}
+     *      }
+     *    )
+     *
+     */
+    public function Update() {
+        return [
+            'error' => 0,
+            'message' => 'Done',
+            'info' => [
+                'id' => 1,
+                'name' => 'A',
+                'address' => 'hanoi'
+            ]
+        ];
+    }
+
+    /**
+     * @OA\Delete(
+     *     path="/users/{id}",
+     *     operationId="deleteUsers",
+     *     tags={"Example"},
+     *     summary="Delete users",
+     *     description="Delete users",
+     *
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="query",
+     *         description="User_id",
+     *         required=true,
+     *         @OA\Schema(
+     *              type="string",
+     *              example="123"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Result info",
+     *         @OA\JsonContent(
+     *            @OA\Property(
+     *              property="result",
+     *              type="boolean",
+     *              example=true
+     *            )
+     *         )
+     *      ),
+     *      security={
+     *          {"api_key_security_example": {}}
+     *      }
+     *    )
+     *
+     */
+    public function Delete() {
         return [
             'error' => 0,
             'message' => 'Done',
